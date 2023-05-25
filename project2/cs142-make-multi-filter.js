6fcf7d6eb21e90ee381559fff40538ad8dd3a5e2
454270186 <sc21ey@leeds.ac.uk>
2023-05-25 17:40:54 +0800

    finish problem 1

--- project2/cs142-make-multi-filter.js
+++ project2/cs142-make-multi-filter.js
@@ -1,3 +1,19 @@
 function cs142MakeMultiFilter(originalArray) {
-    
+    let currentArray = originalArray.slice()
+
+    const arrayFilterer = function (filterCriteria, callback) {
+        if (typeof filterCriteria !== 'function') {
+            return currentArray
+        }
+
+        currentArray = currentArray.filter(filterCriteria)
+
+        if (typeof callback === 'function') {
+            callback.call(originalArray, currentArray)
+        }
+
+        return arrayFilterer
+    }
+
+    return arrayFilterer
 }
